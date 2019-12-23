9be42969cda63a737c187169f5e333d194ec4619
Kateryna Bublyk <kattyB2701@gmail.com>
2019-12-23 07:25:00 +0200

    add vue practice project

---
+++ vue-project/src/components/stocks/Stocks.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <stock
+       v-for="stock in stocks"
+        :key="stock.id"
+        :stock="stock"
+        ></stock>
+  </div>
+</template>
+
+<script>
+import Stock from './Stock.vue'
+export default {
+    data() {
+        return {
+        }
+    },
+    components: {
+        Stock
+    },
+    computed: {
+        stocks() {
+            return this.$store.getters.stocks
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
